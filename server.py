680cec47a4b530011b9d8b9351aa615ba99d858f
KetyaNop <cnop@princeton.edu>
2023-10-08 22:33:50 -0400

    Added "https://haptic-excel.onrender.com/" to cross-origin whitelist

--- server.py
+++ server.py
@@ -35,7 +35,8 @@ app = FastAPI()
 origins = [
     "http://localhost",
     "http://localhost:8000",
-    "http://127.0.0.1:8000/"
+    "http://127.0.0.1:8000/",
+    "https://haptic-excel.onrender.com/"
 ]
 
 app.add_middleware(
